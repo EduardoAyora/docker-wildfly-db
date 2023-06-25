59de5e2e1404f1b1343cc5bc7136362cfd6e23b3
EduardoAyora <eduardoaayora64@gmail.com>
2023-06-25 14:01:36 -0500

    Sin DB

---
+++ src/main/java/ec/edu/ups/demo/helloworld/HelloService.java
@@ -0,0 +1,28 @@
+package ec.edu.ups.demo.helloworld;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.opentracing.Traced;
+
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+
+public class HelloService {
+
+	@Inject
+	@ConfigProperty(name="hello")
+	private String saludo;
+	
+	@Inject
+    private Tracer tracer;
+	
+	@Traced
+    public String createHelloMessage(String name) {
+		Span prepareHelloSpan = tracer.buildSpan("prepare-hello-service").start();
+		String result = saludo + " " + name.toUpperCase() + "!";
+		prepareHelloSpan.finish();
+        return result;
+    }
+
+}
---
+++ src/main/java/ec/edu/ups/demo/helloworld/client/SimpleHelloWorldEndpoint.java
@@ -0,0 +1,33 @@
+package ec.edu.ups.demo.helloworld.client;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.opentracing.Traced;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Path("/proxy")
+public class SimpleHelloWorldEndpoint {
+	
+	@Inject
+    @RestClient
+    SimpleHelloWorldIntf service;
+	
+	 @GET
+	 @Traced(operationName = "cliente-rest-mp")
+	 @Path("/llamarJson")
+	 @Operation(description = "Invocar por medio de cliente microprofile a HelloWorld con respuesta en JSON", summary = "client call getHelloWorldJSON")
+		@APIResponse(responseCode = "200", description = "Saludo respuesta",
+					content = @Content(mediaType = MediaType.APPLICATION_JSON,
+						schema = @Schema(implementation = String.class)))
+	 public String getHello() {
+		 return service.getHello();
+	 }
+
+}
